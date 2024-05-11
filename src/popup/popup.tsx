cb0e0a08552cbfdd708463dfd85d25a259ac4ead
saurabhraj123 <me4saurabh4u@gmail.com>
2024-05-11 17:33:22 +0530

    Basic popup designed

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './popup.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./popup.css";
 
 const App: React.FC<{}> = () => {
   return (
     <div>
-      <img src="icon.png" />
-    </div>
-  )
-}
+      <h1 className="popupHeader">Data Test Id - Inspector</h1>
+      <div className="borderColorPickerContainer">
+        <label htmlFor="borderColorPicker">Border color:</label>
+        <input type="color" id="borderColorPicker" name="borderColorPicker" />
+      </div>
 
-const root = document.createElement('div')
-document.body.appendChild(root)
-ReactDOM.render(<App />, root)
+      <div className="copyIconCheckboxContainer">
+        <label htmlFor="copyIconCheckbox">Show copy icon on hover:</label>
+        <input type="checkbox" id="copyIconCheckbox" name="copyIconCheckbox" />
+      </div>
+
+      <div className="buttonsContainer">
+        <button id="inspectButton">Start</button>
+        <button id="findDuplicates">Find duplicates</button>
+        <button id="findMissing">Find missing</button>
+      </div>
+    </div>
+  );
+};
+
+const root = document.createElement("div");
+document.body.appendChild(root);
+ReactDOM.render(<App />, root);
